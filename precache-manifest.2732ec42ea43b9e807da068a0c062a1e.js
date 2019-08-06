a9e17f6fa8a7c9eb53da5f5228deae0c54089fb9
Joe Van Gundy <vangundyjoe@gmail.com>
2019-08-05 21:38:30 -0400

    Updates

---
+++ precache-manifest.2732ec42ea43b9e807da068a0c062a1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf7ed24a9124859675ed201518c6f70",
+    "url": "/undiscovered-weekly/index.html"
+  },
+  {
+    "revision": "f41327afd04c750c9a88",
+    "url": "/undiscovered-weekly/static/css/main.31549b79.chunk.css"
+  },
+  {
+    "revision": "23a9e664e3f2e4744c77",
+    "url": "/undiscovered-weekly/static/js/2.95f8bab6.chunk.js"
+  },
+  {
+    "revision": "f41327afd04c750c9a88",
+    "url": "/undiscovered-weekly/static/js/main.ec05ce6f.chunk.js"
+  },
+  {
+    "revision": "985611551d5589a05eeb",
+    "url": "/undiscovered-weekly/static/js/runtime~main.1a0b74c4.js"
+  }
+]);
